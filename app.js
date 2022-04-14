cfc5b7fea7cf5c7822fbb9b0ef8792aa1d494ec7
rj-robinjoseph <robinjoseph452@gmail.com>
2022-04-14 22:43:05 +0530

    Working on product section

--- app.js
+++ app.js
@@ -1,8 +1,121 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
+const products = [
+  {
+    id: 1,
+    title: "AIR FORCE",
+    price: 119,
+    desc: "eaque molestiae doloribus aspernatur enim vero quis similique minus",
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "JORDAN",
+    price: 109,
+    desc: "eaque molestiae doloribus aspernatur enim vero quis similique minus",
+    colors: [
+      {
+        code: "gray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "BLAZER",
+    price: 115,
+    desc: "eaque molestiae doloribus aspernatur enim vero quis similique minus",
+    colors: [
+      {
+        code: "white",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "CRATER",
+    price: 99,
+    desc: "eaque molestiae doloribus aspernatur enim vero quis similique minus",
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "white",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "HIPPIE",
+    price: 89,
+    desc: "eaque molestiae doloribus aspernatur enim vero quis similique minus",
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductDesc = document.querySelector(".productDesc");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
+    //change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change text of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$ " + choosenProduct.price;
+    currentProductDesc.textContent = choosenProduct.desc;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //Passing colors for each item
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
   });
 });
